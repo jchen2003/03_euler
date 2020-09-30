a09f67c02159e2edcaa7bfc2c22685df8685f7b8
Jacky <jchen10@stuy.edu>
2020-09-29 21:30:21 -0400

    Finished

---
+++ 1_5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int problem_1(){
+    int sum = 0;
+    int i;
+    for(i=0; i < 1000; i++){
+        if(i%3==0 || i%5 ==0) sum+=i;
+    }
+    return sum;
+}
+
+int problem_5(){
+    int i = 100;
+    int k;
+    int check = 1;
+    while (check){
+        for(k = 2; k <= 20; k++)
+        {
+            if(i%k != 0) break;
+            if(k == 20){
+            check = 0;
+            return i;}
+        }
+        i++;
+    }
+    return -1;
+}
+
+int main(){
+    printf("%d\n",problem_1());
+    printf("%d\n",problem_5());
+}
